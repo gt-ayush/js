e125e8a87a3faf6fda2653768e6c66a53aa1e0f3
gt-ayush <gt-ayush@outlook.com>
2025-11-10 11:40:19 +0530

    loop -- do {}while loop

--- loop.js
+++ loop.js
@@ -3,10 +3,17 @@
 // console.log(i);
 // i++;
 // }
+
+
 let i =0;
-while (i<10) {
-console.log(i);
+// while (i<10) {
+// console.log(i);
+// i++;
 
-i++;
+// }
 
-}
+do {
+    console.log("LPU "+ i);
+    i++;
+
+}while(i<10)
